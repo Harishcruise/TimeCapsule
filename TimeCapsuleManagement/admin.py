182390a5c322483660cc02355143cfa26e1a8336
Ismail Sadiq <olawaleismail@gmail.com>
2024-02-25 18:12:47 -0500

    finalised changes to the models

--- TimeCapsuleManagement/admin.py
+++ TimeCapsuleManagement/admin.py
@@ -1,8 +1,8 @@
 from django.contrib import admin
-from .models import Capsule, CapsuleMedia, Subscription, Comment
+from .models import Capsule, CapsuleContent, Subscription, Comment
 
 # Register your models here.
 admin.site.register(Capsule)
-admin.site.register(CapsuleMedia)
+admin.site.register(CapsuleContent)
 admin.site.register(Subscription)
 admin.site.register(Comment)
--- TimeCapsuleManagement/models.py
+++ TimeCapsuleManagement/models.py
@@ -1,12 +1,13 @@
 from django.db import models
-from django.contrib.auth.models import User
+# from django.contrib.auth.models import User
+from AuthenticationSystem.models import UserProfile
 from django.utils import timezone
 
 # Create your models here.
 
 
 class Capsule(models.Model):
-    owner = models.ForeignKey(User, related_name='capsules', on_delete=models.CASCADE)
+    owner = models.ForeignKey(UserProfile, related_name='capsules', on_delete=models.CASCADE)
     name = models.CharField(max_length=100)
     description = models.TextField(blank=True, null=True)
     creation_date = models.DateTimeField(auto_now_add=True)
@@ -18,18 +19,24 @@ class Capsule(models.Model):
         return self.name
 
 
-class CapsuleMedia(models.Model):
+class CapsuleContent(models.Model):
+    FILE_TYPES = (
+        ('photo', 'Photo'),
+        ('video', 'Video'),
+        ('text', 'Text'),
+    )
+    file_type = models.CharField(max_length=20, choices=FILE_TYPES)
+    file = models.FileField(upload_to='capsule_content/%Y/%m/%d/')
     capsule = models.ForeignKey(Capsule, related_name='media', on_delete=models.CASCADE)
-    file = models.FileField(upload_to='capsule_files/%Y/%m/%d/')
     uploaded_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
-        return f"Media for {self.capsule.name}"
+        return f"File for {self.capsule.name}"
 
 
 class Subscription(models.Model):
-    user = models.ForeignKey(User, related_name='subscriptions', on_delete=models.CASCADE)
-    capsule = models.ForeignKey(Capsule, related_name='subscriptions', on_delete=models.CASCADE)
+    user = models.ForeignKey(UserProfile, related_name='subscriptions', on_delete=models.CASCADE)
+    capsule = models.ForeignKey(Capsule, related_name='subscribers', on_delete=models.CASCADE)
     approved = models.BooleanField(default=False)  # For private capsules, approval needed
     subscribed_on = models.DateTimeField(auto_now_add=True)
 
@@ -39,7 +46,7 @@ class Subscription(models.Model):
 
 class Comment(models.Model):
     capsule = models.ForeignKey(Capsule, related_name='comments', on_delete=models.CASCADE)
-    user = models.ForeignKey(User, related_name='comments_on_capsules', on_delete=models.CASCADE)
+    user = models.ForeignKey(UserProfile, related_name='comments_on_capsules', on_delete=models.CASCADE)
     content = models.TextField()
     timestamp = models.DateTimeField(default=timezone.now)
 
