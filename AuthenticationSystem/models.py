663423ea7289b9d1d557320bfe5df7213a0121ae
Ismail Sadiq <olawaleismail@gmail.com>
2024-02-25 19:55:15 -0500

    Remove phone number field

--- AuthenticationSystem/models.py
+++ AuthenticationSystem/models.py
@@ -5,7 +5,6 @@ from django.contrib.auth.models import User
 
 
 class UserProfile(User):
-    phone_number = models.CharField(max_length=15, blank=True, null=True)
 
     def __str__(self):
         return self.username
