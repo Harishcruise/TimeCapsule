a1f3141663cf06590823acc4740190c47d39ca28
Ismail Sadiq <olawaleismail@gmail.com>
2024-02-26 19:16:49 -0500

    added a create_capsule function

---
+++ TimeCapsuleManagement/crud_operations/capsule_operations.py
@@ -0,0 +1,30 @@
+from django.utils import timezone
+from TimeCapsuleManagement.models import Capsule
+from django.core.exceptions import ObjectDoesNotExist
+
+
+def create_capsule(owner_id, name, description, is_public, unsealing_date):
+    """
+    Create a new capsule.
+    Args:
+        owner_id: The ID of the UserProfile who owns the capsule.
+        name: The name of the capsule.
+        description: A description of the capsule.
+        is_public: Boolean indicating if the capsule is public.
+        unsealing_date: The date and time when the capsule will be unsealed.
+    Returns:
+        The newly created Capsule object or None in case of an error.
+    """
+    try:
+        capsule = Capsule.objects.create(
+            owner=owner_id,
+            name=name,
+            description=description,
+            is_public=is_public,
+            unsealing_date=unsealing_date
+        )
+        return capsule
+    except Exception as e:
+        # Handle exceptions, such as database errors
+        print(f"An error occurred while creating the capsule: {e}")
+        return None
