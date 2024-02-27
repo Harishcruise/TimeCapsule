f3e5cdbe77766600f5abbddd800ea495096264dc
anaikarsaad <anaikarsaad@gmail.com>
2024-02-26 23:18:32 -0500

    model for search component

--- SearchCapsule/models.py
+++ SearchCapsule/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+# search_capsule/models.py
+from django.db import models
+from TimeCapsuleManagement.models import Capsule
+
+class SearchableCapsule(models.Model):
+    capsule = models.OneToOneField(Capsule, on_delete=models.CASCADE, primary_key=True)
+    # Add any additional fields for search functionality
+
+    def __str__(self):
+        return f"Searchable {self.capsule.name}"
+
+
