a92cdb9743ea4c580cb9d609f769176d31b862a1
Ismail Sadiq <olawaleismail@gmail.com>
2024-03-10 11:58:28 -0400

    Modified the form to accept file contents as well

--- TimeCapsuleManagement/views.py
+++ TimeCapsuleManagement/views.py
@@ -4,6 +4,7 @@ from django.contrib import messages
 from .models import Capsule, CapsuleContent, Comment, Subscription
 from AuthenticationSystem.models import UserProfile
 from .forms import CapsuleForm
+import mimetypes
 
 
 # Create your views here.
@@ -17,7 +18,23 @@ def my_capsules(request):
     if request.method == 'POST':
         form = CapsuleForm(request.POST, request.FILES)
         if form.is_valid():
-            form.save()
+            capsule = form.save()
+            # Handle the uploaded files for CapsuleContent
+            files = request.FILES.getlist('capsule_contents')
+            for file in files:
+                mime_type, _ = mimetypes.guess_type(file.name)
+                file_type = 'text'  # Default file type
+                if mime_type:
+                    if mime_type.startswith('image/'):
+                        file_type = 'photo'
+                    elif mime_type.startswith('video/'):
+                        file_type = 'video'
+                # Create a CapsuleContent object for each file
+                CapsuleContent.objects.create(
+                    file=file,
+                    capsule=capsule,
+                    file_type=file_type,  # Use the determined file type
+                )
             messages.success(request, 'Time Capsule created successfully!')
             return redirect('TimeCapsuleManagement:my_capsules')
     else:
