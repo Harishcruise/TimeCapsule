49f31f1243447c551aa6333eb725f4d187933d6a
shashankkannan <135678936+shashankkannan@users.noreply.github.com>
2024-03-11 19:21:31 -0400

    Update models.py

--- AuthenticationSystem/models.py
+++ AuthenticationSystem/models.py
@@ -1,11 +1,11 @@
-from django.db import models
-from django.contrib.auth.models import User, AbstractUser
-
-# Create your models here.
-
-
-class UserProfile(User):
-    bio = models.TextField(null=True, blank=True)
-
-    def __str__(self):
-        return self.username
+# from django.db import models
+# from django.contrib.auth.models import User, AbstractUser
+#
+# # Create your models here.
+#
+#
+# class UserProfile(User):
+#     bio = models.TextField(null=True, blank=True)
+#
+#     def __str__(self):
+#         return self.username
