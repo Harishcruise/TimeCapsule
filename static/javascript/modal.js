3cbcbeacda53b1a134c80a0e9a2587943882c242
Harishcruise <53746760+Harishcruise@users.noreply.github.com>
2024-03-16 15:33:57 -0400

    Redesigned Header and Modal

---
+++ static/javascript/modal.js
@@ -0,0 +1,11 @@
+
+function closeModal(modal_id){
+  const modal = document.getElementById(modal_id)
+  modal.style.display='none';
+}
+
+
+function openModal(modal_id) {
+  const modal = document.getElementById(modal_id);
+  modal.style.display='block';
+}
