121a1e280d7c67eb09d92696be9a19fc93617b75
Ismail Sadiq <olawaleismail@gmail.com>
2024-03-17 07:59:32 -0400

    added functionality to update files as well

--- TimeCapsuleManagement/urls.py
+++ TimeCapsuleManagement/urls.py
@@ -6,6 +6,7 @@ urlpatterns = [
     path('', views.home, name='home'),
     path('my-capsules/', views.my_capsules, name='my_capsules'),
     path('edit/<int:capsule_id>/', views.edit_capsule, name='edit_capsule'),
+    path('capsule-contents/<int:capsule_id>/', views.get_capsule_contents, name='get_capsule_contents'),
     path('delete/<int:capsule_id>/', views.delete_capsule, name='delete_capsule'),
     path('post_comment/<int:capsule_id>/', views.post_comment, name='post_comment')
 ]
