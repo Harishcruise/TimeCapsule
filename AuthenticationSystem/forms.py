220bfe8d90804d62cdf14a9d806f9d165a92c5f8
shashankkannan <135678936+shashankkannan@users.noreply.github.com>
2024-03-18 19:54:08 -0400

    Signup update 1

--- AuthenticationSystem/forms.py
+++ AuthenticationSystem/forms.py
@@ -12,4 +12,4 @@ class CustomSignupForm(forms.Form):
     first_name = forms.CharField(widget=forms.TextInput(attrs={'class': 'form-input'}))
     last_name = forms.CharField(widget=forms.TextInput(attrs={'class': 'form-input'}))
     email = forms.EmailField(widget=forms.EmailInput(attrs={'class': 'form-input'}))
-    bio = forms.CharField(widget=forms.Textarea(attrs={'class': 'form-input'}))
+    bio = forms.CharField(widget=forms.Textarea(attrs={'class': 'form-input', 'style': 'height: 50px;'}))
